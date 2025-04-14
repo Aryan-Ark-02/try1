b365553cb7e1f5e153156dab4c753a527e4852b0
Aryan-Ark-02 <Arksharma1211@gmail.com>
2025-04-14 15:22:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flagit remote add origin <your_github_repo_url>
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint, send_file
 sk, render_template, request, redirect, url_for, flash, Blueprint, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
